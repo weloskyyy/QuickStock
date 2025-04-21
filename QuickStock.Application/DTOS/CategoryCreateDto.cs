28c33089d03ea64a33da1cc695244ee07af1dc0a
weloskyyy <juan.welosky@gmail.com>
2025-04-20 20:43:15 -0400

    Crud Actualizado

--- QuickStock.Application/DTOS/CategoryCreateDto.cs
+++ QuickStock.Application/DTOS/CategoryCreateDto.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QuickStock.Application.DTOS
 {
@@ -13,5 +8,4 @@ namespace QuickStock.Application.DTOS
         [StringLength(50)]
         public string Name { get; set; }
     }
-
 }
