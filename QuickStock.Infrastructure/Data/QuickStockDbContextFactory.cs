78ed97b82fa67209ef5d1b605a72bee9d87d5bd2
weloskyyy <juan.welosky@gmail.com>
2025-04-24 00:36:57 -0400

    Hice una nueva migracion

---
+++ QuickStock.Infrastructure/Data/QuickStockDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using QuickStock.Infrastructure.Data;
+
+namespace QuickStock.Infrastructure
+{
+    public class QuickStockDbContextFactory : IDesignTimeDbContextFactory<QuickStockDbContext>
+    {
+        public QuickStockDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<QuickStockDbContext>();
+
+            optionsBuilder.UseSqlServer("Server=WELOSKKYY\\MSSQLSERVER02;Database=QuickStockDb;Trusted_Connection=True;TrustServerCertificate=True;");
+
+            return new QuickStockDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ QuickStock.Infrastructure/Migrations/20250424031813_CleanSetupWithSale.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuickStock.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CleanSetupWithSale : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalAmount",
+                table: "Sales",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalAmount",
+                table: "Sales");
+        }
+    }
+}
